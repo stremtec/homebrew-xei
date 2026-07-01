bcfeb8b19da8cc9fc20f54adb509f0dffa89b4f7
asill-livestream <copii.list@gmail.com>
2026-07-01 17:47:13 +0900

    Bump to v0.1.4

--- Formula/xei.rb
+++ Formula/xei.rb
@@ -1,16 +1,16 @@
 class Xei < Formula
   desc "xei (晴) — a modern Vim-like terminal editor in Rust"
   homepage "https://github.com/stremtec/xei"
-  version "0.1.1"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/stremtec/xei/releases/download/v0.1.1/xei-aarch64-apple-darwin.gz"
-      sha256 "b9de93b96744763c3b3a532a574e194bb561d6a7817446c11d1b33fce61643db"
+      url "https://github.com/stremtec/xei/releases/download/v0.1.4/xei-aarch64-apple-darwin.gz"
+      sha256 "b2f407f94a8d73cef5bda1f86f6d8404a2b1a5f1d306e549514f3f623a491305"
     else
-      url "https://github.com/stremtec/xei/releases/download/v0.1.1/xei-x86_64-apple-darwin.gz"
-      sha256 "1d7cd15f73a4cc600401ada6f62d18a4e0e09ee2967eedeeb6f7305e175aef29"
+      url "https://github.com/stremtec/xei/releases/download/v0.1.4/xei-x86_64-apple-darwin.gz"
+      sha256 "e740edeff5c405b7c989607e782ebee62189cdf5f483b4b96ebc5b2d50009089"
     end
   end
 
